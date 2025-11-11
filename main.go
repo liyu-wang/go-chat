a81a9c6aeaf72ac2f207783296bcd1a034d8498e
liyu-wang <liyuwang1985@gmail.com>
2025-11-11 23:45:02 +1100

    Creating and using rooms

--- main.go
+++ main.go
@@ -23,8 +23,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-
+	http.Handle("/room", r)
+	// Get the room running
+	// this will start the room's main event loop in the background to handle clients
+	// joining, leaving and message forwarding, which allows the main goroutine to run the web server
+	go r.run()
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
